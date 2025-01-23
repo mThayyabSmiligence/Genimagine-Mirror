6ec5a3c6ec02d82ae9e1ac640e4c785a0b979ada
Mohan-raj02 <mohanrajp1715@gmail.com>
2025-01-23 11:28:01 +0530

    testing

--- back-end/app.js
+++ back-end/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const app = express();
 const path = require('path');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
+
+console.log("hello world");
 
 app.listen(process.env.PORT,() => {
     console.log(`server listening to port ${process.env.PORT} in ${process.env.NODE_ENV}`)
-})
+});
