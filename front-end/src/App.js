acd47b87a588f00421cbc1ac704516f02354baa0
Mohan-raj02 <mohanrajp1715@gmail.com>
2025-01-23 18:54:00 +0530

    Tested HTTP cokkie with jwt token

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+    get_cookie()
+  },[])
+
+
+    const get_cookie=async()=>{
+    await axios.get("http://localhost:3001/get-token",
+    {
+      withCredentials:true,
+    })
+    .then((response)=>{
+      console.log(response)
+    })
+    .catch(
+      (error)=>{
+        console.error("error :"+error.response?error:error);
+      }
+    )
+  }
+
+  const post_cookie=async()=>{
+    await axios.post("http://localhost:3001/post-token",{},
+    {
+      withCredentials:true,
+    }).then(()=>console.log("post request")).catch(
+      (error)=>{
+        console.error("error :"+error.response?error:error);
+      }
+    )
+  }
+
   return (
     <div className="App">
-
-        
+      <button onClick={()=>{post_cookie()}}>post</button>
+         
     </div>
   );
 }
