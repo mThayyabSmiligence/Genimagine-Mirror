14ee0ed593178ef4029f60ba16caa4d9f1a08e50
Mohan-raj02 <mohanrajp1715@gmail.com>
2025-01-30 15:47:53 +0530

    generated image for user

--- back-end/service/GenerateImageService.js
+++ back-end/service/GenerateImageService.js
@@ -1,25 +1,70 @@
 const dotenv =require('dotenv')
 const path =require('path')
+const jwt =require('jsonwebtoken')
+const db = require('../config/connectDatabase');
+const axios =require('axios')
 
 dotenv.config({path: path.join(__dirname, 'config', 'config.env')})
-
+ 
 
 const cloud_flare_acc_id= process.env.CLOUD_FLARE_ACC_ID
 const cloud_flare_api_key=process.env.CLOUD_FLARE_API_KEY
 
-
-const canUserGenerateFree=async(data,type)=>{
+exports.handleGenerateImageUser = async(data, token) => {                                                      
+    const model_data=handelModel(data.model)
     
+    const decodedtoken=jwt.decode(token)
+    
+
+    console.log("model data :"+model_data.model_url)
+    const image =await generateImageUser(data.prompt,model_data.model_url)
+    if(!image){return false}        
+    await increaseFreeGenerationCount(decodedtoken.id )
+    return image
+
 }
 
-const generateImageUser = async(userData,inputsData) => {
-    const inputs=""
+exports.canUserGenerateFree=async(id)=>{
+    try{
+        const query = "SELECT free_generation_count FROM users WHERE user_id = ?"
+        const [data] =await db.execute(query, [id]);
+        
+        if(data[0].free_generation_count<20){
+            console.log(typeof(data[0].free_generation_count))
+            return true
+        }else{
+            return false
+    
+        }
+    }catch(err){
+        console.log(err )
+    }
+}
+
+
+const increaseFreeGenerationCount=async(id)=>{
+    try{
+        const query = "UPDATE users SET free_generation_count = free_generation_count + 1, last_free_generated = NOW() WHERE user_id = ?;"
+        const [data] =await db.execute(query, [id]);
+        console.log("free image generation count for user is increased")
+        return true;
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+
+const generateImageUser = async(inputs,model_url) => {
+  
 
     try{
 
         const response = await axios.post(
-            `https://api.cloudflare.com/client/v4/accounts/${cloud_flare_acc_id}/ai/run/@cf/bytedance/stable-diffusion-xl-lightning`,
-            inputs,
+            `https://api.cloudflare.com/client/v4/accounts/${cloud_flare_acc_id}/ai/run/${model_url}`,
+            {
+                prompt:inputs
+            },
             {
               headers: {
                 'Authorization': `Bearer ${cloud_flare_api_key}`,
@@ -32,12 +77,7 @@ const generateImageUser = async(userData,inputsData) => {
         // const decodedString= atob(response.data.result.image)
         // const imageBuffer = Uint8Array.from(decodedString,(m)=>m.codePointAt(0))
         const imageBuffer= Buffer(response.data,'base64')
-        if (type == "guest") {
-            const guestImageCount = GuestImageCount(data);     
-        }else if(type=="user"){
         
-            console.log('user image count is increased')
-        }
         return imageBuffer
     }catch(error){
         console.log("error generating images:" +error)
@@ -58,7 +98,7 @@ const handelModel=(modelType)=>{
         }
         
         case 2:{
-            model_url=process.env.MODEL_3
+            model_url=process.env.MODEL_2   
             cp_required=5;
             break
         }
