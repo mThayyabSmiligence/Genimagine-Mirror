8c283dfb87a94c6bdbb6856369b173ade500e6d8
Mohan-raj02 <mohanrajp1715@gmail.com>
2025-02-06 09:00:43 +0530

    added api for email otp

--- back-end/routes/AuthenticationRoute.js
+++ back-end/routes/AuthenticationRoute.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const {userLogin,userRegister,userLogout} = require('../controller/UsersController')
+const {userLogin,userRegister,userLogout, emailOtpRequest, verifyEmailOtp} = require('../controller/UsersController')
 const router = express.Router();
 
-router.route("/login",).post(userLogin)
+router.route("/login").post(userLogin)
+router.route("/email-otp-request").post(emailOtpRequest)
+router.route('/email-otp-verify').post(verifyEmailOtp)
 router.route('/register').post(userRegister);
 router.route('/logout').get(userLogout)
 
