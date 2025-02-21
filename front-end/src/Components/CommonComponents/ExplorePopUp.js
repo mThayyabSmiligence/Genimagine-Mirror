e17c647703b0bde4c150644581d0ce323dda98ab
Mohan-raj02 <mohanrajp1715@gmail.com>
2025-02-21 17:38:24 +0530

    like and view has fetched

--- front-end/src/Components/CommonComponents/ExplorePopUp.js
+++ front-end/src/Components/CommonComponents/ExplorePopUp.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../../Css/ExplorePopUp.css'
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import { axiosNoAUth, axiosPrivate } from '../../API\'s/axios';
 
 function ExplorePopUp({ image, onClose }) {
+  const hasViewed = useRef(false); // Prevents multiple calls
+
+  useEffect(() => {
+    if (!hasViewed.current) {
+      showview();
+      hasViewed.current = true; // Set flag to true after first call
+    }
+    console.log(image);
+  }, []);
+
+  const showview = async() => {
+
+    try{
+      const response = await axiosNoAUth.post(`explore/${image.published_id}/view`)
+      console.log(response)
+      if(response.data.success){
+        image.views_count++
+        console.log("View count updated successfully")
+        console.log(response.data)
+      }
+    }catch(error){
+      console.log(error)
+  }
+  }
+  const addLikes = async() => {
+    try{
+      const response = await axiosPrivate.post(`/explore/${image.published_id}/like`)
+      console.log(response)
+      if(response.data.success){
+        image.likes_count++
+        console.log("Like count updated successfully")
+        console.log(response.data)
+      }
+    }catch(error){
+      console.log(error)
+    } 
+  }
+
+  const removeLike = async() => {
+    try{
+      const response = await axiosPrivate.post(`/explore/${image.published_id}/unlike`)
+      console.log(response)
+      if(response.data.success){
+        image.likes_count--
+        console.log("Like count updated successfully")
+        console.log(response.data)
+      }
+    }catch(error){
+      console.log(error)
+    }
+  } 
+    
+
   return (
     <div className="explore-popup-overlay">
       <div className="explore-popup-content">
@@ -31,9 +85,9 @@ function ExplorePopUp({ image, onClose }) {
 
           </div>
 
-          <div className='explore-metrics d-flex  justify-content-end'>
+          <div className='explore-metrics d-flex justify-content-end'>
             <div className='user-like-container'>
-              <button className=' button light-button user-like-button d-flex align-items-center'><FavoriteBorderIcon/>  <p className="ms-2 m-0">{image.likes_count}</p></button>
+              <button onClick={() => addLikes()} className=' button light-button user-like-button d-flex align-items-center'><FavoriteBorderIcon/>  <p className="ms-2 m-0">{image.likes_count}</p></button>
             </div>
             <div className='user-view-container mx-2'>
               <button className='button light-button user-view-button d-flex align-items-center'><VisibilityIcon/><p className="ms-2 m-0 ">{image.views_count}</p></button>
