ead448046e7730dd73e4f16ab5482dcebc860f6c
mnothingm30 <mnothingm30@gmail.com>
2025-02-25 17:52:54 +0530

    fixed a small issue in side navbar

--- front-end/src/Layouts/NavLayout.js
+++ front-end/src/Layouts/NavLayout.js
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom'
 export default function NavLayout({setShowNavBar,showNavBar,width}) {
   return (
     <div className='nav-layout'>
-        <SideNavBar showNavBar={showNavBar} setShowNavBar={setShowNavBar}></SideNavBar>
+        <SideNavBar showNavBar={showNavBar} setShowNavBar={setShowNavBar} width={width}></SideNavBar>
         <SubTopbar setShowNavBar={setShowNavBar} showNavBar={showNavBar} width={width}></SubTopbar>
         <div className={`content-section ${!showNavBar?"big":"short"}  d-flex flex-column` } style={{ width:`${width<766?width:showNavBar?width-250:width}px` }}>
 
