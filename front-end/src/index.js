ec9217a1ce7f437b34006e873b0af00f8a4f4a0e
Mohan-raj02 <mohanrajp1715@gmail.com>
2025-02-26 18:37:12 +0530

    slider added

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { AuthProvider } from './Context/AuthProvider';
 import { RefreshDataProvider } from './Context/RefreshDataProvider';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 
