d8d44ec4344427501b51a6fb5e584322d3979b9d
mnothingm30 <mnothingm30@gmail.com>
2025-03-04 13:53:52 +0530

    redirects guest user to login page when clicking buy credits button

--- front-end/src/Components/CreditPurchase/CreditPurchaseCard.js
+++ front-end/src/Components/CreditPurchase/CreditPurchaseCard.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../../Context/AuthProvider';
+import { Link } from 'react-router-dom';
 
 function CreditPurchaseCard({ data,buyCredits}) {
+
+    const {loggedIn}= useContext(AuthContext)
 
     const handelBuy=(e)=>{
         buyCredits(data.package_id,null,e)
@@ -12,7 +16,12 @@ function CreditPurchaseCard({ data,buyCredits}) {
                 <p className="card-text"><strong>{data.credits}</strong> Credits</p>
                 <p className="card-text text-success">₹{data.cost}</p>
                 <p className="text-muted">{data.description}</p>
-                <button onClick={(e)=>handelBuy(e)} className="button dark-button">Buy Now</button>
+                {
+                    loggedIn?
+                    <button onClick={(e)=>handelBuy(e)} className="button dark-button">Buy Now</button>
+                    :
+                    <Link to={"/login"}  className="button dark-button ">Buy Now</Link>
+                }
             </div>
         </div>
     );
