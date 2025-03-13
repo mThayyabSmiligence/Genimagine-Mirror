a7580ea8df11eaec1d50086ef00b7aed7d6cbe2b
mnothingm30 <mnothingm30@gmail.com>
2025-03-13 16:14:56 +0530

    now images are uploaded to aws s3

--- back-end/service/ExploreService.js
+++ back-end/service/ExploreService.js
@@ -27,23 +27,10 @@ exports.publishToExploreService=async(image_id,caption,token,user_id)=>{
         }
     }
 
-    try{
-        const response = await uploadImageToExplore(user_id,generated_image_data.image_path,token)
-        if(response.status!= 200){
-            return response
-        }
-        image_data=response
 
-    }catch( err){
-        return{
-            status:500,
-            message:"internal server error",
-            success:false
-        }
-    }
     try{
         const query ="INSERT INTO explore (user_id,prompt,model, caption,image_id, image_url, image_path,resolution,aspect_ratio) VALUES (?, ?,?, ?, ?,?,?,?,?)"
-        const [rows] = await db.execute(query,[user_id,generated_image_data.prompt,generated_image_data.model,caption,image_id,image_data.imageUrl,image_data.imagePath,generated_image_data.resolution,generated_image_data.aspect_ratio])
+        const [rows] = await db.execute(query,[user_id,generated_image_data.prompt,generated_image_data.model,caption,image_id,generated_image_data.image_url,generated_image_data.image_path,generated_image_data.resolution,generated_image_data.aspect_ratio])
 
         if (rows.affectedRows == 0) {
              return {
