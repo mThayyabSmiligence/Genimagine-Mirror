c6634586d67214c1c86904b8db94a991bf248084
mnothingm30 <mnothingm30@gmail.com>
2025-03-18 11:31:07 +0530

    fixed the chat delete api

--- front-end/src/Components/CommonComponents/ChatOptionDropDown.js
+++ front-end/src/Components/CommonComponents/ChatOptionDropDown.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../../Css/ChatOptionDropDown.css'
 
-function ChatOptionDropDown({handleRename}) {
+function ChatOptionDropDown({handleRename,handleDelete}) {
   return (
     <div className="list-option-menu p-0 d-flex flex-column justify-content-center">
         <button onClick={()=>handleRename()} className="list-option-item " >Rename</button>
-        <button className="list-option-item">Delete</button>
+        <button onClick={()=>{handleDelete()}} className="list-option-item">Delete</button>
     </div>
   )
 }
