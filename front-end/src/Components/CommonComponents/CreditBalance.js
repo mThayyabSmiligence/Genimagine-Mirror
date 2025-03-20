bcd84b2a9531c014353eb2a1014ffde55d6eb4df
mnothingm30 <mnothingm30@gmail.com>
2025-03-20 15:15:13 +0530

    added function which generated the test emailsand chaneged the ui of link to buy credits page

--- front-end/src/Components/CommonComponents/CreditBalance.js
+++ front-end/src/Components/CommonComponents/CreditBalance.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react'
 import RefreshDataContext from '../../Context/RefreshDataProvider'
 import "../../Css/SubTopBar.css"
+import { Link } from 'react-router-dom'
+import AddIcon from '@mui/icons-material/Add';
 
 export default function CreditBalance() {
     const [creditBalance,setCreditBalance]=useState(0)
@@ -14,6 +16,7 @@ export default function CreditBalance() {
     <div className='credit-balance-container d-flex align-items-center ' title='credit points' >
         <span className='credit-balance-coin' ><p>C</p></span>
         <p className='credit-balance-value mx-2'>{creditBalance||1000}</p>
+        <Link to="/credit-purchase" className='link'><span className='plus-symbol'><AddIcon className='icon'/></span></Link>
     </div>
   )
 }
