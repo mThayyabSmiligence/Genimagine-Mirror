1b953b522c61b50bdb1e4a96aa5dc3514fc39809
Mohan-raj02 <mohanrajp1715@gmail.com>
2025-03-25 09:36:30 +0530

    style preview added on the prompt setting

--- front-end/src/Pages/Explore/ExploreImageDetail.js
+++ front-end/src/Pages/Explore/ExploreImageDetail.js
@@ -18,7 +18,48 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeletePopUp from "../../Components/CommonComponents/DeletePopUp";
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
-
+const styleList = [
+    { id: 1, style_name: "Textured Oil Painting", },
+    { id: 2, style_name: "Chalk and Charcoal", },
+    { id: 3, style_name: "Chinese Ink Painting", },
+    { id: 4, style_name: "Realism", },
+    { id: 5, style_name: "3D Render", },
+    { id: 6, style_name: "Ink & Wash", },
+    { id: 7, style_name: "Bright and Exaggerated Cartoon World", },
+    { id: 8, style_name: "Anime", },
+    { id: 9, style_name: "Black & White", },
+    { id: 10, style_name: "Bokeh",  },
+    { id: 11, style_name: "Cinematic",  },
+    { id: 12, style_name: "Comic Book",  },
+    { id: 13, style_name: "Film Noir",  },
+    { id: 14, style_name: "Indian Miniature",  },
+    { id: 15, style_name: "Japanese Ukiyo-e",  },
+    { id: 16, style_name: "Neon Glow",  },
+    { id: 17, style_name: "Pixel Art",  },
+    { id: 18, style_name: "Steampunk",  },
+    { id: 19, style_name: "Baroque Portrait",  },
+    { id: 20, style_name: "Cyberpunk Setting",  },
+    { id: 21, style_name: "Delicate Watercolor Painting",  },
+    { id: 22, style_name: "Dreamlike and Abstract Composition",  },
+    { id: 23, style_name: "Dynamic Graffiti Artwork",  },
+    { id: 24, style_name: "Gothic Horror Setting",  },
+    { id: 25, style_name: "High Dynamic Range Photography",  },
+    { id: 26, style_name: "Monochrome Sketch",  },
+    { id: 27, style_name: "Moody Gothic Atmosphere",  },
+    { id: 28, style_name: "Mythical World",  },
+    { id: 29, style_name: "Pencil Sketch Style",  },
+    { id: 30, style_name: "Playful Cartoon Style",  },
+    { id: 31, style_name: "Pop Art Style",  },
+    { id: 32, style_name: "Richly Detailed Baroque Style",  },
+    { id: 33, style_name: "Soft Watercolor Style",  },
+    { id: 34, style_name: "Surreal Landscape",  },
+    { id: 35, style_name: "80s inspired vaporwave style",  },
+    { id: 36, style_name: "Thick Oil Painting Style",  },
+    { id: 37, style_name: "Ultra Realistic HDR Style",  },
+    { id: 38, style_name: "Urban Street Art Style",  },
+    { id: 39, style_name: "Vaporwave Aesthetic",  },
+    { id: 40, style_name: "Vibrant Pop Art Illustration",  }
+]
 function ExploreImageDetail() {
     const location = useLocation()
     const { published_id } = useParams(); 
@@ -35,11 +76,7 @@ function ExploreImageDetail() {
     const [imagePathCopied, setImagePathCopied] = useState(false);
     const [isUsersImage, setIsUsersImage] = useState(location.pathname.includes("/u/published-images"));
     const [showDeletePopUp, setShowDeletePopUp] = useState(false);
-    
-
     const Navigate = useNavigate();
-
-
 
         useEffect(() => {
             ImageDetail();
@@ -114,12 +151,15 @@ function ExploreImageDetail() {
       }
 
     const handleCopy = (image) => {
+        const style = styleList.find((style) => imageData?.style == style.style_name)
+
         localStorage.setItem("image_settings", JSON.stringify(
           {
             model: image.model,
             aspectRatio: {
               aspectRatio: image.aspect_ratio
-            }
+            },
+            style: style.id
           }
         ))
         setRefreshImageSettings(!refreshImageSettings)
